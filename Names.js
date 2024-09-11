e016d1584a94d41e0bb67e04adc32d7de8422e51
Reshmafareed <163650782+Reshmafareed@users.noreply.github.com>
2024-09-11 11:54:48 +0500

    Add files via upload

---
+++ Names.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Reshma', 'noor', 'aj ameen', 'rabia'];
+const message = 'Welcome to Typescript class: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Names.ts
@@ -0,0 +1,9 @@
+let names: string[] = ['Reshma','noor','aj ameen','rabia'];
+
+const message: string ='Welcome to Typescript class: '
+
+for(let i=0; i<names.length; i++){
+
+console.log(message + names[i]);
+
+}
